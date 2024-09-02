d1478068b211f72ccb1ca258d64f75326a4a82e8
changhyoun <llh94367@naver.com>
2024-09-02 17:51:42 +0900

    2024-09-02 2-17 complete

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -27,10 +27,12 @@ const KakaoMap = ({ mapX, mapY }) => {
       });
     };
 
-    // Kakao 지도 초기화
     const initializeMap = (kakao) => {
+      // DOM 요소가 있는지 확인
+      const container = document.getElementById('map');
+      if (!container) return;
+
       kakao.maps.load(() => {
-        const container = document.getElementById('map');
         const options = {
           center: new kakao.maps.LatLng(mapY, mapX),
           level: 3,
@@ -38,35 +40,29 @@ const KakaoMap = ({ mapX, mapY }) => {
 
         const map = new kakao.maps.Map(container, options);
 
-        // 마커 이미지 설정
         const markerImage = new kakao.maps.MarkerImage(
-          CampDetails_main_map_coordinate, // 이미지 경로
-          new kakao.maps.Size(70, 70), // 마커 이미지 크기
-          { offset: new kakao.maps.Point(45, 75) } // 마커의 좌표에 해당하는 이미지 위치
+          CampDetails_main_map_coordinate,
+          new kakao.maps.Size(70, 70),
+          { offset: new kakao.maps.Point(45, 75) }
         );
 
-        // 마커 설정
         const markerPosition = new kakao.maps.LatLng(mapY, mapX);
         const marker = new kakao.maps.Marker({
           position: markerPosition,
-          image: markerImage, // 마커 이미지 설정
+          image: markerImage,
         });
 
         marker.setMap(map);
       });
     };
 
-    // 스크립트 로드 및 지도 초기화
     loadKakaoMapScript()
       .then(initializeMap)
       .catch((error) => {
         console.error('Failed to load Kakao Map script', error);
       });
 
-    return () => {
-      // 컴포넌트가 언마운트될 때 스크립트를 제거하지 않고 유지
-      // 이는 다른 페이지로 이동 후 다시 돌아올 때 지도가 재사용될 수 있도록 합니다.
-    };
+    // 스크립트를 유지하되, 상태가 변경될 때마다 초기화
   }, [mapX, mapY]);
 
   return <div id="map" className='CampDetails_main_map'></div>;
